3fe45c11ce8609470021f2ad4d8220c0d5677359
Valentin Thirion <valentinthirion@gmail.com>
2016-03-22 13:20:32 +0100

    Update __openerp__.py

--- __openerp__.py
+++ __openerp__.py
@@ -7,8 +7,9 @@
     'category': 'Product',
     'description': 
     """
-    This modules adds some functionalities to the products of Odoo. 
-    - it checks if the internal reference and the EAN13 Barcode already exists
+    Product uniqueness on Reference and EAN13
+
+    This modules adds some functionalities to the products of Odoo, it checks if the internal reference and the EAN13 Barcode already exists
 
     This module has been developed by Bernard Delhez, intern @ AbAKUS it-solutions, under the control of Valentin Thirion.
     """,
