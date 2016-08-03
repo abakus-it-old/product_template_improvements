f9188aa9950782a3724ad4f15371b5e23493181b
Valentin Thirion <valentinthirion@gmail.com>
2016-08-03 12:10:20 +0200

    Changed 'onchange' to 'constraints' and change 'return warning' to 'raise Validationerror'

--- model/product_template.py
+++ model/product_template.py
@@ -1,9 +1,10 @@
 from openerp import models, fields, api
+from openerp.exceptions import validationError
 
 class product_template_improvements(models.Model):
     _inherit = ['product.template']
-    
-    @api.onchange('default_code')
+
+    @api.constraints('default_code')
     def check_default_code(self):
         if self.default_code and len(self.default_code)>0:
             cr = self.env.cr
@@ -11,9 +12,9 @@ class product_template_improvements(models.Model):
             product_template_obj = self.pool.get('product.template')
             product_templates = product_template_obj.search(cr, uid, [('default_code', '=', self.default_code)])
             if product_templates:
-                return {'warning': {'title': 'Internal Reference Failure', 'message': 'The internal reference already set on another product. As this field is supposed to be unique, please enter another one.'},}
+                raise ValidationError('The internal reference already set on another product. As this field is supposed to be unique, please enter another one.')
     
-    @api.onchange('barcode')
+    @api.constraints('barcode')
     def check_barcode(self):
         if self.barcode and len(self.barcode)>0:
             cr = self.env.cr
@@ -21,4 +22,4 @@ class product_template_improvements(models.Model):
             product_template_obj = self.pool.get('product.template')
             product_templates = product_template_obj.search(cr, uid, [('barcode', '=', self.barcode)])
             if product_templates:
-                return {'warning': {'title': 'Barcode Failure', 'message': 'The  barcode is already set on another product. As this field is supposed to be unique, please enter another one.'},}
+                raise ValidationError('The  barcode is already set on another product. As this field is supposed to be unique, please enter another one.')
